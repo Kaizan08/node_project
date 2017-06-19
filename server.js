da950142b25967fbb47983537220a043d013e268
Kaizan08 <jlsnyder08@gmail.com>
2017-06-19 16:48:00 -0500

    added functionality to put spaces in the paragraphs

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const lorem = require('lorem-ipsum');
-var output = lorem({count:3, units:'paragraph'});
+var output = lorem({count:3, units:'paragraph', format: 'html'});
 
 app.get('/lorem/:id', function(req, res){
     var id = req.params.id;
-    output = lorem({count:id, units:'paragraph'});
+    output = lorem({count:id, units:'paragraph', format: 'html'});
     res.send(output);
 });
 app.get('/lorem', function(req, res){
